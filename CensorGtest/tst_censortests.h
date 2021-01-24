ec70a8518acfbfa06877dab73ac6b887f42ebd6f
Denis Kashitsyn <kamikadze1024@yandex.ru>
2021-01-24 22:00:42 +0300

    First workable Censor gtest.
    Signed-off-by: Denis Kashitsyn <kamikadze1024@yandex.ru>

--- CensorGtest/tst_censortests.h
+++ CensorGtest/tst_censortests.h
@@ -12,17 +12,39 @@ TEST(CensorGtests, CensorTests)
     ASSERT_THAT(0, Eq(0));
 }
 
-//включаю файл с функцией, которую буду тестировать
-#include "../Censor/Functions.hpp"
+//включаю файлы, которые буду тестировать
+#include "../Censor/argsparser.hpp"
 
-TEST(CensorGtests, test1) {
-    bool res = simpleTestWor();
-    ASSERT_EQ(res, true);
+TEST(CensorGtests, argHelpTest) {
+    //кол-во строк
+    int argc = 2;
+
+    //0 первая строка
+    char firstStr[100] = "/home/user/directory/executableFile";
+
+    //1 вторая строка
+    char secondStr[100] = "--help";
+
+    char *argv[2];
+    argv[0] = firstStr;
+    argv[1] = secondStr;
+
+    try {
+        //Парсер аргументов командной строки
+        std::shared_ptr<Arguments::ArgsParser> argPars
+                = Arguments::ArgsParser::getInstance();
+
+        //паршу аргументы
+        argPars->processCommandLineOptions(argc, argv);
+
+        //если это хелп:
+        ASSERT_EQ(true, argPars->isHelp());
+    } catch (Arguments::ArgsParserException &exc) {
+        std::cout << exc.what() << std::endl;
+        FAIL();
+    }
 }
 
-TEST(CensorGtests, test2) {
-    bool res = simpleTestWor();
-    ASSERT_EQ(res, true);
-}
+
 
 #endif // TST_CENSORTESTS_H
