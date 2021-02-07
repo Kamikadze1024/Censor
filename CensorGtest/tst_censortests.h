597a308f9dec14fe24824c68e8009ea41c790e00
Denis Kashitsyn <kamikadze1024@yandex.ru>
2021-02-07 22:08:33 +0300

    Added notworkable test.
    Signed-off-by: Denis Kashitsyn <kamikadze1024@yandex.ru>

--- CensorGtest/tst_censortests.h
+++ CensorGtest/tst_censortests.h
@@ -109,4 +109,45 @@ TEST(CensorGtests, argHelpTest3) {
     }
 }
 
+TEST(CensorGtests, argHelpTest4) {
+    //кол-во строк
+    int argc = 3;
+
+    //0 первая строка
+    char firstStr[100] = "/home/user/directory/executableFile";
+
+    //1 вторая строка
+    char secondStr[100] = "-h";
+
+    //2 третья строка
+    char thirdStr[100] = "-IinputFile.txt";
+
+    char *argv[3];
+    argv[0] = firstStr;
+    argv[1] = secondStr;
+    argv[2] = thirdStr;
+
+    try {
+        //Парсер аргументов командной строки
+        std::shared_ptr<Arguments::ArgsParser> argPars
+                = Arguments::ArgsParser::getInstance();
+
+        //паршу аргументы
+        argPars->processCommandLineOptions(argc, argv);
+
+        //если это хелп:
+        ASSERT_EQ(true, argPars->isHelp());
+
+        //если имя файла не выдает - валимся
+        //получить путь к входному файлу
+        std::string inputFileName = argPars->getInputFileName();
+
+        std::string neStr = "";
+        ASSERT_STREQ(neStr.c_str(), inputFileName.c_str());
+    } catch (Arguments::ArgsParserException &exc) {
+        std::cout << exc.what() << std::endl;
+        FAIL();
+    }
+}
+
 #endif // TST_CENSORTESTS_H
