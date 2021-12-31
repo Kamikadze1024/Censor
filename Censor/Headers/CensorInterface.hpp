414c0dc975ce00f47914e7116f83e65035c5a47b
Denis Kashitsyn <kamikadze1024@yandex.ru>
2022-01-01 02:18:29 +0300

    Added censor interface header.
    Signed-off-by: Denis Kashitsyn <kamikadze1024@yandex.ru>

---
+++ Censor/Headers/CensorInterface.hpp
@@ -0,0 +1,27 @@
+/*
+ * Абстрактный класс интерфейса цензора
+ * Кашицын Денис <kamikadze1024@yandex.ru>
+ */
+#ifndef CENSORINTERFACE_HPP
+#define CENSORINTERFACE_HPP
+
+class CensorInterface {
+public:
+    /*
+     * Установить подстановки
+     */
+
+    /*
+     * Установит словарь запретных слов
+     */
+
+    /*
+     * Установить текст
+     */
+
+    /*
+     * Процензурить
+     */
+};
+
+#endif // CENSORINTERFACE_HPP
